5fa8fe2abacd5f826f3caa4a6c243b2bb2f6c6d0
Adriano Tsurumaki <adrianotsurumaki@gmail.com>
2026-03-04 18:05:10 -0300

    feat: add dark mode with useDarkMode composable and ThemeToggle

---
+++ app/javascript/components/ThemeToggle.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { Moon, Sun } from 'lucide-vue-next'
+import { useDarkMode } from '@/composables/useDarkMode'
+import { Button } from '@/components/ui/button'
+
+const { isDark, toggleDark } = useDarkMode()
+</script>
+
+<template>
+  <Button variant="ghost" size="icon" @click="toggleDark()">
+    <Sun v-if="isDark" class="size-5" />
+    <Moon v-else class="size-5" />
+  </Button>
+</template>
