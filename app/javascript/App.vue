4b47be863015bdd7699ebecf90d2a03d391a500d
Adriano Tsurumaki <adrianotsurumaki@gmail.com>
2026-03-11 19:16:25 -0300

    feat: add toast notification system with vue-sonner

--- app/javascript/App.vue
+++ app/javascript/App.vue
@@ -1,14 +1,17 @@
 <script setup lang="ts">
-  import { onMounted } from 'vue'
-  import { useAuthStore } from '@stores/auth'
+import { onMounted } from 'vue'
+import { useAuthStore } from '@stores/auth'
+import 'vue-sonner/style.css'
+import { Toaster } from '@components/ui/sonner'
 
-  const authStore = useAuthStore()
+const authStore = useAuthStore()
 
-  onMounted(() => {
-    authStore.loadToken()
-  })
+onMounted(() => {
+  authStore.loadToken()
+})
 </script>
 
 <template>
   <RouterView />
+  <Toaster />
 </template>
